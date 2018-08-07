0eb2601e6f68758256898175537088f9cab362bf
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-08-07 13:04:03 +0300

    Update

--- src/main/java/idk/plugin/bossbartext/BossBarText.java
+++ src/main/java/idk/plugin/bossbartext/BossBarText.java
@@ -1,13 +1,7 @@
 package idk.plugin.bossbartext;
 
 import cn.nukkit.Player;
-import cn.nukkit.Server;
-import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.plugin.PluginManager;
-import cn.nukkit.utils.Config;
-import cn.nukkit.utils.TextFormat;
-import cn.nukkit.utils.DummyBossBar;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
@@ -15,8 +9,7 @@ import cn.nukkit.event.player.PlayerJoinEvent;
 public class BossBarText extends PluginBase implements Listener {
 
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents((Listener)this, (Plugin)this);
-        this.getLogger().info(TextFormat.GREEN + "BossBarText enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
         this.getConfig();
     }
@@ -24,12 +17,8 @@ public class BossBarText extends PluginBase implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-	      String text = "\u00A7l" + this.getConfig().getString("text");
-        p.createBossBar(text, 100);
-    }
-
-    @Override
-    public void onDisable() {
-        this.getLogger().info(TextFormat.RED + "BossBarText disabled!");
+        String text = this.getConfig().getString("text").replace("§", "\u00A7");
+        int length = this.getConfig().getInt("length", 100);
+        p.createBossBar(text, length);
     }
 }
