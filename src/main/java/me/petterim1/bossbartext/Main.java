36d57daf2e45ae390ee6e9ead9776027a829cb6e
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-12-22 14:13:55 +0200

    Boss bar colors now supported on 1.18

--- src/main/java/me/petterim1/bossbartext/Main.java
+++ src/main/java/me/petterim1/bossbartext/Main.java
@@ -4,11 +4,14 @@ import cn.nukkit.event.player.PlayerLocallyInitializedEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.utils.BossBarColor;
+import cn.nukkit.utils.DummyBossBar;
 
 public class Main extends PluginBase implements Listener {
 
     private String text;
     private int length;
+    private BossBarColor color;
 
     @Override
     public void onEnable() {
@@ -16,10 +19,18 @@ public class Main extends PluginBase implements Listener {
         this.saveDefaultConfig();
         this.text = this.getConfig().getString("text").replace("%n", "\n");
         this.length = this.getConfig().getInt("length", 100);
+        try {
+            this.color = BossBarColor.valueOf(this.getConfig().getString("color"));
+        } catch (IllegalArgumentException ignore) {
+        }
     }
 
     @EventHandler
     public void onJoin(PlayerLocallyInitializedEvent e) {
-        e.getPlayer().createBossBar(this.text, this.length);
+        DummyBossBar.Builder builder = new DummyBossBar.Builder(e.getPlayer());
+        if (color != null) {
+            builder.color(color);
+        }
+        e.getPlayer().createBossBar(builder.text(text).length((float) length).build());
     }
 }
